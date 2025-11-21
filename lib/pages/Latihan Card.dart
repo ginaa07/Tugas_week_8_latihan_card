5aab664bf3db1dd6150192986a98505bd902b812
ginaa07 <1123150124@global.ac.id>
2025-11-21 13:36:04 +0700

    menambah borderall dan colors white didalam decoration

--- lib/pages/Latihan Card.dart
+++ lib/pages/Latihan Card.dart
@@ -114,10 +114,21 @@ class LatihanCard extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Container(
-                        decoration: BoxDecoration(shape: BoxShape.circle),
+                        decoration: BoxDecoration(shape: BoxShape.circle, border: Border.all(color: Colors.white,width: 4)),
+                        
                         child: CircleAvatar(
                           radius: 60,
                           backgroundImage: AssetImage("assets/images/imagesui.png"),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      //text Name
+                      Text(
+                        "Regina Safarina",
+                        style: TextStyle(
+                          fontSize: 26.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
                         ),
                       ),
                   
