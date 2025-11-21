ba0f958beb0c33f46b2faf46993966f4b763a6ac
ginaa07 <1123150124@global.ac.id>
2025-11-21 12:07:26 +0700

    perubahan pada sizedbox untuk text

--- lib/pages/Latihan Card.dart
+++ lib/pages/Latihan Card.dart
@@ -107,13 +107,32 @@ class LatihanCard extends StatelessWidget {
                     colors: [Colors.yellow, Colors.red],
                   ),
                 ),
-                child: Text("ini uji coba",
-                style: TextStyle(
-                  fontSize: 20.0
+                child: Row(
+                children: [
+                  //Todo
+                  //container dengan circle
+                  Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.green)
+                    ),
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage("assets/images/imagesui.png"),
+                    ),
+                  ),
+                  SizedBox(height:  5),
+                  Text("text",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold
+                  ),
+                  ),
+                  
+
+                ],
+                )           
                 ),
                 ),
-              ),
-            )
           ],
 
         ),
